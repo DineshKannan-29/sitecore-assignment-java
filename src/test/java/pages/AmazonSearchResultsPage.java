7e73b08e97d8e38e6343f9fcb73060647d180489
DineshKannan-29 <dinesh29.91@gmail.com>
2021-08-02 14:28:50 +0200

    added to check total products vs first product

--- src/test/java/pages/AmazonSearchResultsPage.java
+++ src/test/java/pages/AmazonSearchResultsPage.java
@@ -1,9 +1,15 @@
 package pages;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import stepDefinitions.StepDefinitions;
+
+import java.util.List;
 
 import static utils.Utils.getPageTile;
 import static utils.Utils.waitUntilElementToBeClickable;
@@ -14,15 +20,24 @@ public class AmazonSearchResultsPage {
 
     WebElement productLinkInSearchList;
 
+    private static final Logger logger = LogManager.getLogger(AmazonSearchResultsPage.class);
+
     public AmazonSearchResultsPage(WebDriver webDriver) {
         this.driver = webDriver;
         PageFactory.initElements(driver, this);
     }
 
     public AmazonProductsDetailsPage selectProduct(String productNumber) {
-        productLinkInSearchList = driver.findElement(By.xpath("(//a[@class=\"a-link-normal a-text-normal\"])[" + productNumber + "]"));
-        waitUntilElementToBeClickable(driver, productLinkInSearchList);
-        productLinkInSearchList.click();
+        List<WebElement> searchList = driver.findElements(By.xpath("//a[@class=\"a-link-normal a-text-normal\"]"));
+        logger.info("Total products retrieved in search : "+searchList.size());
+        if (Integer.parseInt(productNumber) <= (searchList.size())){
+            productLinkInSearchList = driver.findElement(By.xpath("(//a[@class=\"a-link-normal a-text-normal\"])[" + productNumber + "]"));
+            waitUntilElementToBeClickable(driver, productLinkInSearchList);
+            productLinkInSearchList.click();
+        } else {
+            throw new RuntimeException("Could not find the product number : "+productNumber+" from the search list since search result contains only "+ (searchList.size()) +" products");
+        }
+
         return new AmazonProductsDetailsPage(driver);
     }
 
